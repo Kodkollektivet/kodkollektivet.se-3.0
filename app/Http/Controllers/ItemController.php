860abb19723fd5e1129450c49ae53df14481a6ba
confidentlyDumb <alexandrabushure@gmail.com>
2022-12-14 19:10:35 +0100

    Fixed model search (case sensitivity issue)

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -8,12 +8,13 @@ use App\Http\Controllers\ImageController;
 
 class ItemController extends Controller
 {
-    public function imageRequestStore(object $item, array $images, string $type) {
-
+    public function imageRequestStore(object $item, array $images, string $type)
+    {
         $store        = new Request(['images' => $images, 'type' => ($type == 'image' ? 'item_cover' : $type)]);
         $delete_image = isset($item->$type) ? $item->$type : null;
 
-        if (isset($delete_image)) {
+        if (isset($delete_image))
+        {
             $delete         = new Request();
             $delete->images = array($delete_image);
             $delete->type   = $type == 'image' ? 'item_cover' : $type;
@@ -24,10 +25,12 @@ class ItemController extends Controller
         return ImageController::store($item, $store);
     }
 
-    public function renderCode(string $string) {
+    public function renderCode(string $string)
+    {
         $string_data = ItemController::replaceSafe($string);
 
-        if ($string_data->allow) {
+        if ($string_data->allow)
+        {
             $text = substr($string, $string_data->start, $string_data->end);
             $code = str_replace(' --!', '">', str_replace('!-- end --!', '</code></pre></div>', str_replace('!-- start', '<div class="mockup-code tw-ignore"><pre><code class="', $text)));
 
@@ -39,10 +42,12 @@ class ItemController extends Controller
         return $string;
     }
 
-    public function hideSnippet(string $string) {
+    public function hideSnippet(string $string)
+    {
         $string_data = ItemController::replaceSafe($string);
         
-        if ($string_data->allow) {
+        if ($string_data->allow)
+        {
             $string = str_replace(substr($string, $string_data->start, $string_data->end - 24), "[ CODE SNIPPET ]&nbsp;", $string);
             ItemController::hideSnippet($string);
         }
@@ -50,37 +55,43 @@ class ItemController extends Controller
         return $string;
     }
 
-    public function normalizeHTML(string $html, $setBreaks = false) {
-
+    public function normalizeHTML(string $html, $setBreaks = false)
+    {
         $html = str_replace('<br><br>', '<br>', $html);
         
-        while (substr_count($html, '<br><br>')) {
+        while (substr_count($html, '<br><br>'))
+        {
             $html = str_replace(['<br><br>'], '<br>', $html);
         }
 
         return !$setBreaks ? str_replace('<br>', PHP_EOL, $html) : str_replace(PHP_EOL, '<br>', $html);
     }
 
-    public function prepareLink($item_name) {
-
+    public function prepareLink($item_name)
+    {
         return str_replace(['?', '&', '='], '', str_replace(' ', '-', $item_name));
     }
 
-    public function getModel($name) {
-        $model = '\App\Models\\'. ucfirst($name);
+    public function getModel($name)
+    {
+        if (class_exists('\App\Models\\'.ucfirst($name))) {
+            return '\App\Models\\'.ucfirst($name);
+        } elseif (class_exists('\App\Models\\'.$name)) {
+            return '\App\Models\\'.$name;
+        }
 
-        return class_exists($model) ? $model : null;
+        return null;
     }
 
-    public function getController($name) {
-        
+    public function getController($name)
+    {
         $controller = '\App\Http\Controllers\\'. ucfirst($name) .'Controller';
         
         return class_exists($controller) ? $controller : null;
     }
 
-    private function replaceSafe(string $string) {
-        
+    private function replaceSafe(string $string)
+    {
         $start_count = substr_count($string, '!-- start');
         $end_count   = substr_count($string, '!-- end --!');
         $start_index = strpos($string, '!-- start');
